4701cd1fd2992e347b55c16e9dc820e5ea0c5c6c
catburgg <catburgg@github.com>
2025-12-12 02:26:00 +0800

    minor modification

---
+++ human_plan/dataset_preprocessing/otv_isaaclab/hf_dataset_fixed_set/generate_dataset_hands.sh
@@ -0,0 +1,20 @@
+DATASET_ROOT=/mnt/project/public/world_model/RawData/EgoVLA_SIM
+SAVE_PATH=/mnt/project/public/world_model/RawData/processed_EgoVLA_SIM
+
+additional_tag=FIXED_SET_MIX
+
+frame_skip=1
+
+# Now 30 Hz
+CHUNKS=16
+for IDX in {0..15}; do
+    python human_plan/dataset_preprocessing/otv_isaaclab/hf_dataset_fixed_set/generate_hand_parquets.py \
+        --num_chunk $CHUNKS  --additional_tag $additional_tag  \
+        --clip_starting 0  \
+        --chunk_id $IDX --frame_skip $frame_skip --sample_skip 1 --future_len 61 \
+        --dataset_root $DATASET_ROOT \
+        --save_path $SAVE_PATH &
+done
+wait
+
+python human_plan/dataset_preprocessing/otv_isaaclab/parquet_to_dataset_hand.py --additional_tag $additional_tag --save_path $SAVE_PATH
---
+++ human_plan/dataset_preprocessing/otv_isaaclab/hf_dataset_fixed_set/generate_dataset_image.sh
@@ -0,0 +1,16 @@
+DATASET_ROOT=/mnt/project/public/world_model/RawData/EgoVLA_SIM
+SAVE_PATH=/mnt/project/public/world_model/RawData/processed_EgoVLA_SIM
+
+CHUNKS=32
+for IDX in {0..31}; do
+    CUDA_VISIBLE_DEVICES=$(($IDX % 8)) python human_plan/dataset_preprocessing/otv_isaaclab/generate_img_parquets.py \
+        --num_chunk $CHUNKS  --clip_starting 0 \
+        --chunk_id $IDX --frame_skip 1 \
+        --dataset_root $DATASET_ROOT \
+        --save_path $SAVE_PATH &
+done
+wait
+
+python human_plan/dataset_preprocessing/otv_isaaclab/parquet_to_dataset_images.py --save_path $SAVE_PATH
+
+python human_plan/dataset_preprocessing/otv_isaaclab/generate_images_mapping.py --save_path $SAVE_PATH
